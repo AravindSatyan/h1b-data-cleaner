cffa64e028e73382006addd6c81740906b1e2ef1
Aravind Satyanarayanan <aravind@gmail.com>
2024-07-26 13:22:31 -0400

    final changes uploaded to PyPl

--- PycharmProjects/python_package/h1b-data-clean/setup.py
+++ PycharmProjects/python_package/h1b-data-clean/setup.py
@@ -1,24 +1,18 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name='data_cleaner',
+    name='cleanh1b',  # Update this to a unique name
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
     ],
-    entry_points={
-        'console_scripts': [
-            'data_cleaner=data_cleaner.data_clean:main',
-        ],
-    },
     author='Aravind Satyanarayanan',
     author_email='aravind.bedean@gmail.com',
     description='A package for cleaning data frames.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/data_cleaner',
+    url='https://github.com/AravindSatyan/h1b-data-cleaner',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
